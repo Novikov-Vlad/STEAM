2b3cb8753f9f7aeee6c12b220d52d1cfab5398e0
Novikov-Vlad <55314104+Novikov-Vlad@users.noreply.github.com>
2022-02-04 21:49:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,215 @@
+function initMap() {
+    // Create a new StyledMapType object, passing it an array of styles,
+    // and the name to be displayed on the map type control.
+    const styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "featureType": "all",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "weight": "2.00"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#9c9c9c"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#f2f2f2"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 45
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#eeeeee"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#7b7b7b"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#46bcec"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#c8d7d4"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#070707"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            }
+        ],
+      { name: "Styled Map" }
+    );
+    // Create a map object, and include the MapTypeId to add
+    // to the map type control.
+    const uluru = { lat: -25.363, lng: 131.044 };
+    const map = new google.maps.Map(
+      document.getElementById("map"),
+      {
+        zoom: 4,
+        center: uluru,
+      }
+    );
+  
+    const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +'<div class="img">'+
+    "</div>" +
+    '<p><h2>Act Big! Ultimate Music  Festival</h2></p>' +
+    '<div id="bodyContent">' +
+    '<p><h4>Pattichion Municipal Amphitheater </br> <small> Nikolaou G. Dimitriou Street, Larnaca,6031,Cyprus</small></h4></p>' +
+    "<p><h5>An evening of bowling by geeks for geeks.</br> Nothing more, nothing less (adorn bowling shirts — funny shoes provided) Follow @TeamLebowski or #GeekBowling on Twitter for up to the minute news...</h5></p>" +
+    "</div>" +
+    "</div>";
+  
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+    
+  
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map,
+      title: "Uluru (Ayers Rock)",
+    });
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set("styled_map", styledMapType);
+    map.setMapTypeId("styled_map");
+  }
